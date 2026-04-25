b1a82c43ca6e6ef92c63c719e8abfe5ba20d03ca
Hikl845 <freelancer198756@gmail.com>
2026-04-26 01:44:15 +0300

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/example/urlShortener/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.urlShortener.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handle(BadRequestException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
