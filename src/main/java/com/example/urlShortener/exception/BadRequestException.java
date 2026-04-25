24e2adad779ac923f3c765eaf6d9f93274b71f29
Hikl845 <freelancer198756@gmail.com>
2026-04-26 01:43:19 +0300

    Update BadRequestException.java

--- src/main/java/com/example/urlShortener/exception/BadRequestException.java
+++ src/main/java/com/example/urlShortener/exception/BadRequestException.java
@@ -1,1 +1,7 @@
+package com.example.urlShortener.exception;
 
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
