ad939c5743466cede1b826f00dd252db7e9575f6
Hikl845 <freelancer198756@gmail.com>
2026-04-26 01:39:38 +0300

    Update AuthController.java

--- src/main/java/com/example/urlShortener/auth/AuthController.java
+++ src/main/java/com/example/urlShortener/auth/AuthController.java
@@ -8,18 +8,17 @@ public class AuthController {
 
     private final AuthService authService;
 
-    // 🔥 ДОДАЙ ЦЕЙ КОНСТРУКТОР
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
 
     @PostMapping("/register")
-    public void register(@RequestBody AuthRequest request) {
-        authService.register(request);
+    public AuthResponse register(@RequestBody @Valid RegisterRequest request) {
+        return authService.register(request);
     }
 
     @PostMapping("/login")
-    public AuthResponse login(@RequestBody AuthRequest request) {
+    public AuthResponse login(@RequestBody @Valid LoginRequest request) {
         return authService.login(request);
     }
 }
