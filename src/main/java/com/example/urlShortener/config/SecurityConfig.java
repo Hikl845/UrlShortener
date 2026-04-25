92dac1c0c00aa9f05d8d49dc9a07b2cac4b71253
Hikl845 <freelancer198756@gmail.com>
2026-04-26 01:51:38 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/urlShortener/config/SecurityConfig.java
+++ src/main/java/com/example/urlShortener/config/SecurityConfig.java
@@ -3,17 +3,6 @@ package com.example.urlShortener.config;
 import com.example.urlShortener.auth.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-ipackage com.example.urlShortener.config;
-
-import com.example.urlShortener.auth.JwtAuthFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
