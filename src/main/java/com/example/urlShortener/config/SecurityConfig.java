7a7e2eb4eef8273a88ecc73653f0a05f0f80eb4c
Hikl845 <freelancer198756@gmail.com>
2026-04-26 22:22:36 +0300

    Update SecurityConfig.java

--- src/main/java/com/example/urlShortener/config/SecurityConfig.java
+++ src/main/java/com/example/urlShortener/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.example.urlshortener.config;
 
-import com.example.urlShortener.auth.JwtAuthFilter;
+import com.example.urlshortener.auth.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
