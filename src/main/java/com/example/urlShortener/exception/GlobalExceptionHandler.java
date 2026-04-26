adf8fb6084a8ceffded323bd0b2dd76fc3e628f8
Hikl845 <freelancer198756@gmail.com>
2026-04-26 22:18:00 +0300

    Update GlobalExceptionHandler.java

--- src/main/java/com/example/urlShortener/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/urlShortener/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.urlShortener.exception;
+package com.example.urlshortener.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
