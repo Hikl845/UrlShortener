d4d48c36d65e26c3db11a035d96ca66182e7a333
Hikl845 <freelancer198756@gmail.com>
2026-04-26 22:19:36 +0300

    Update UrlShortenerApplication.java

--- src/main/java/com/example/urlShortener/UrlShortenerApplication.java
+++ src/main/java/com/example/urlShortener/UrlShortenerApplication.java
@@ -1,4 +1,4 @@
-package com.example.urlShortener;
+package com.example.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
