7cc1099a53cc76f48c9caf69d2178fe2cacc4efb
Hikl845 <freelancer198756@gmail.com>
2026-04-26 22:18:50 +0300

    Update LinkRepository.java

--- src/main/java/com/example/urlShortener/link/LinkRepository.java
+++ src/main/java/com/example/urlShortener/link/LinkRepository.java
@@ -1,4 +1,4 @@
-package com.example.urlShortener.link;
+package com.example.urlshortener.link;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
