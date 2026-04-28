995a663d96f5976d59da9fc7830590fc841d0ec8
Hikl845 <freelancer198756@gmail.com>
2026-04-28 03:55:54 +0300

    Update JwtAuthFilter.java

--- src/main/java/com/example/urlshortener/auth/JwtAuthFilter.java
+++ src/main/java/com/example/urlshortener/auth/JwtAuthFilter.java
@@ -36,7 +36,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
         final String authHeader = request.getHeader("Authorization");
 
-        // 🔴 1. Якщо немає токена — пропускаємо
+        
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
@@ -47,21 +47,21 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         try {
             final String username = jwtService.extractUsername(token);
 
-            // 🔴 2. Якщо користувач знайдений і ще не аутентифікований
+            
             if (username != null &&
                     SecurityContextHolder.getContext().getAuthentication() == null) {
 
-                // 🔴 3. Перевірка валідності токена
+                
                 if (jwtService.isValid(token)) {
 
-                    // 🔴 4. Дістаємо користувача з БД
+                    
                     User user = userRepository.findByUsername(username)
                             .orElseThrow();
 
-                    // 🔴 5. Дістаємо роль з токена
+                    
                     String role = jwtService.extractRole(token);
 
-                    // 🔴 6. Створюємо authentication з ролями
+                    
                     UsernamePasswordAuthenticationToken auth =
                             new UsernamePasswordAuthenticationToken(
                                     user, // 🔥 тепер передаємо User
@@ -69,13 +69,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                     List.of(new SimpleGrantedAuthority(role))
                             );
 
-                    // 🔴 7. Додаємо деталі
+                    
                     auth.setDetails(
                             new WebAuthenticationDetailsSource()
                                     .buildDetails(request)
                     );
 
-                    // 🔴 8. Кладемо в SecurityContext
+                    ]
                     SecurityContextHolder
                             .getContext()
                             .setAuthentication(auth);
@@ -83,7 +83,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             }
 
         } catch (Exception e) {
-            // 🔴 якщо токен битий — просто пропускаємо (не падаємо)
+            
         }
 
         filterChain.doFilter(request, response);
